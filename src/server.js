d0b65be6b6d07208e65050d0945733da91a7c1d7
Andrey Carvalho <andreycarvalho@icloud.com>
2020-08-06 21:07:27 +0000

    heroku

--- src/server.js
+++ src/server.js
@@ -103,7 +103,4 @@ server
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
-.set('port', (process.env.PORT || 8080))
-.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-  });
+.set('port', (process.env.PORT || 8080));
