1cbfaa3742d1afab4b60113506a037a44268d334
Andrey Carvalho <andreycarvalho@icloud.com>
2020-08-07 20:49:41 +0000

    dat 4

---
+++ src/pages.js
@@ -0,0 +1,100 @@
+const Database = require("./database/db")
+
+const {subjects, weekdays, getSubject} = require("./utils/format")
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+async function  pageStudy(req, res) {
+    const filters = req.query
+    
+    if (!filters.subject || !filters.weekdays || !filters.time) {
+        return res.render("study.html", {filters, subjects, weekdays})
+    }
+
+    const timeToMinutes = convertHoursToMinutes(filters.time)
+    const query = `
+        SELECT *
+        FROM proffys p
+        JOIN classes c ON (c.proffy_id = c.id)
+        WHERE EXISTS (
+            SELECT class_schedule.*
+            FROM class_schedule
+            WHERE class_schedule.class_id = "1"
+            AND class_schedule.weekday = ${filters.weekdays}
+            AND class_schedule.time_from <= ${timeToMinutes}
+            AND class_schedule.time_to > ${timeToMinutes}
+        )
+        AND classes.subject = "${filters.subject}"
+    
+    `
+    try {
+        const db = await Database
+        const proffys = await db.all(query)
+
+        proffys.map((proffy) => {
+            proffy.subject = getSubject(proffy.subject)
+        })
+
+        return res.render("study.html", {proffys, subjects, filters, weekdays})
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+    return res.render("study.html", {proffys, filters, subjects, weekdays})
+}
+
+
+function pageGiveClasses(req, res) { 
+    return res.render("give-classes.html", {subjects, weekdays})
+}
+
+async function saveClasses(req, res) {
+    const createProffy = require("./database/createProffy")
+    
+    const proffyValue = {
+        name: req.body.name,
+        avatar: req.body.avatar,
+        whatsapp: req.body.whatsapp,
+        bio: req.body.bio
+
+    }
+
+    const classValue = {
+        subject: req.body.subject,
+        cost: req.body.cost
+    }
+
+    const classScheduleValues = req.body.weekdays.map(
+        (weekday, index) => {
+            return {
+                weekday,
+                time_from: convertHoursToMinutes(req.body.time_from[index]),
+                time_to: convertHoursToMinutes(req.body.time_to[index])
+            }
+        
+    })
+    
+    try {
+        const db = await Database
+        await createProffy(db, {proffyValue, classValue, classScheduleValues})
+        
+        let queryString = "?subject=" + req.body.subject
+        queryString += "&weekday=" + req.body.weekday[0]
+        queryString += "&time=" + req.body.time_from[1]
+
+        return res.redirect("/study" + queryString)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+module.exports = {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+}
